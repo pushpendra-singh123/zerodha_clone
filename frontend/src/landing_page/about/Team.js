6889f9336999b99fbaa0759fb632d4c0005d906c
pushpendra-singh123 <pushpendra.singh4_cs22@gla.ac.in>
2025-07-06 19:17:12 +0530

    home-page

---
+++ frontend/src/landing_page/about/Team.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Team() {
+  return <h1>Hero </h1>;
+}
+
+export default Team;
---
+++ frontend/src/landing_page/support/CreateTicket.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function CreateTicket() {
+  return <h2>Create Ticket</h2>;
+}
+
+export default CreateTicket;
