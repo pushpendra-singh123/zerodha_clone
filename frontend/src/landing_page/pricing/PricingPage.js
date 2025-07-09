4f7e75f68428d95d83dcf0b83c72bc5b7546cb59
pushpendra-singh123 <pushpendra.singh4_cs22@gla.ac.in>
2025-07-09 11:52:16 +0530

    pricing

--- frontend/src/landing_page/pricing/PricingPage.js
+++ frontend/src/landing_page/pricing/PricingPage.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Hero from "./Hero";
-import OpenAccount from "./../OpenAccount";
 import Brokerage from "./Brokerage";
+import OpenAccount from "../OpenAccount";
 
 function PricingPage() {
   return (
