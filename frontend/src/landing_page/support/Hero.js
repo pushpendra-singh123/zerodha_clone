2915a1d3080576884330c6aa7bb983f64c7f2d65
pushpendra-singh123 <pushpendra.singh4_cs22@gla.ac.in>
2025-07-19 17:14:25 +0530

    update hero.js in landing page

--- frontend/src/landing_page/support/Hero.js
+++ frontend/src/landing_page/support/Hero.js
@@ -12,12 +12,24 @@ function Hero() {
           <h1 className="fs-3">
             Search for an answer or browse help topics to create a ticket
           </h1>
-          <input placeholder="Eg. how do I activate F&O" />
+          <input
+            placeholder="Eg. how do I activate F&O, why is my order getting rejected"
+            className="h-38 p-10 mb-15  rounded-0"
+          />
           <br />
-          <a href="">Track account opening</a>
-          <a href="">Track segment activation</a>
-          <a href="">Intraday margins</a>
-          <a href="">Kite user manual</a>
+          <a href="" className="pe-5">
+            Track account opening
+          </a>
+          <a href="" className="pe-5">
+            Track segment activation
+          </a>
+          <a href="" className="pe-5">
+            Intraday margins
+          </a>
+          <br />
+          <a href="" className="pe-5">
+            Kite user manual
+          </a>
         </div>
         <div className="col-6 p-3">
           <h1 className="fs-3">Featured</h1>
