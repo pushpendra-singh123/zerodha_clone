823a56551ef6860a9711f2bfe9c0bfbdbea4552e
pushpendra-singh123 <pushpendra.singh4_cs22@gla.ac.in>
2025-08-03 15:34:00 +0530

    [pushpendra] connecting mongodb with express

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,18 @@
+require("dotenv").config();
+
 const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const PORT = process.env.PORT || 3002;
+const uri = process.env.MONGO_URL;
+
 const app = express();
 
-app.listen(3002, () => console.log("app start"));
+app.listen(PORT, () => {
+  console.log("App started!");
+  mongoose
+    .connect(uri)
+    .then(() => console.log("DB connected!"))
+    .catch((err) => console.error("DB connection error:", err));
+});
