0b28d9f4cb1544903f492cb350e70eb35549bdbb
pushpendra-singh123 <pushpendra.singh4_cs22@gla.ac.in>
2025-08-03 16:22:05 +0530

    Creating schemas and models

--- backend/index.js
+++ backend/index.js
@@ -2,12 +2,18 @@ require("dotenv").config();
 
 const express = require("express");
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 const cors = require("cors");
 
 const PORT = process.env.PORT || 3002;
 const uri = process.env.MONGO_URL;
 
 const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+
+
 
 app.listen(PORT, () => {
   console.log("App started!");
