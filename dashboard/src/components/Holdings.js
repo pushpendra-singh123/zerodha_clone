d190eefcedbb6ff8dbb6a2dc08b9f79fe93b2590
pushpendra-singh123 <pushpendra.singh4_cs22@gla.ac.in>
2025-08-04 16:37:10 +0530

    [pushpendra] Plotting graph using chart.js

--- dashboard/src/components/Holdings.js
+++ dashboard/src/components/Holdings.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import { VerticalGraph } from "./VerticalGraph";
 
 const Holdings = () => {
   const [allHoldings, setAllHoldings] = useState([]);
@@ -9,6 +10,19 @@ const Holdings = () => {
       setAllHoldings(res.data);
     });
   }, []);
+
+  const labels = allHoldings.map((subArray) => subArray["name"]);
+
+  const data = {
+    labels,
+    datasets: [
+      {
+        label: "Stock Price",
+        data: allHoldings.map((stock) => stock.price),
+        backgroundColor: "rgba(255, 99, 132, 0.5)",
+      },
+    ],
+  };
 
   return (
     <>
@@ -69,6 +83,7 @@ const Holdings = () => {
           <p>P&L</p>
         </div>
       </div>
+      <VerticalGraph data={data} />
     </>
   );
 };
